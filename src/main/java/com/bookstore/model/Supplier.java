a57407e2f4e5c8357d00a6e9b95728863431e0f5
zoown12 <zoown12@gmail.com>
2024-07-17 16:09:11 +0900

    GUI 완성

--- src/main/java/com/bookstore/model/Supplier.java
+++ src/main/java/com/bookstore/model/Supplier.java
@@ -1,4 +1,54 @@
 package main.java.com.bookstore.model;
 
 public class Supplier {
+	
+	private String supplierId;
+	private String name;
+	private String phone;
+	private String email;
+	private String adress;
+	private String supplierName;
+	public String getSupplierId() {
+		return supplierId;
+	}
+	public void setSupplierId(String supplierId) {
+		this.supplierId = supplierId;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getAdress() {
+		return adress;
+	}
+	public void setAdress(String adress) {
+		this.adress = adress;
+	}
+	public String getSupplierName() {
+		return supplierName;
+	}
+	public void setSupplierName(String supplierName) {
+		this.supplierName = supplierName;
+	}
+	@Override
+	public String toString() {
+		return "Supplier [supplierId=" + supplierId + ", name=" + name + ", phone=" + phone + ", email=" + email
+				+ ", adress=" + adress + ", supplierName=" + supplierName + "]";
+	}
+	
+	
 }
