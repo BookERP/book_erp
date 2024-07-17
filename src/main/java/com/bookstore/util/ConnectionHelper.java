2ddaa3fbc2927d0f8662ce17d34b859ef7a79293
zoown12 <zoown12@gmail.com>
2024-07-17 14:57:18 +0900

    TEST

---
+++ src/main/java/com/bookstore/util/ConnectionHelper.java
@@ -0,0 +1,29 @@
+package main.java.com.bookstore.util;
+
+// ("jdbc:oracle:thin:@DB이름_medium?TNS_ADMIN=지갑폴더경로",UserID,UserPW); 클라우드 DB 설정
+//, "ADMIN", "Madwolves9810!"
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class ConnectionHelper {
+	//함수 (접근자 : public , static)
+	public static Connection getConnection(String dsn) {
+		Connection conn = null;	
+		try {
+			if(dsn.equalsIgnoreCase("mysql")){
+				Class.forName("com.mysql.jdbc.Driver");
+				conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/SampleDB","sozoo","mysql");
+			}
+				else if(dsn.equalsIgnoreCase("oracle")) {
+				
+				Class.forName("oracle.jdbc.OracleDriver");
+				conn = DriverManager.getConnection("jdbc:oracle:thin:@bookerpmsa_high?TNS_ADMIN=C:/oracle/cloud/Wallet_BookERPMSA", "ADMIN", "Madwolves9810!");
+				System.out.println("connection sucess!!");
+			}
+		}catch(Exception e){
+			e.printStackTrace();
+		}finally {
+			return conn;
+		}
+	}
+}
