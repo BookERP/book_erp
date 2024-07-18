669df87c084b364751f0237851e727d0bfca25e7
ruukr8080@gmail.com <ruukr8080@gmail.com>
2024-07-18 18:45:42 +0900

    create MainFrame

--- src/main/java/com/bookstore/Main.java
+++ src/main/java/com/bookstore/Main.java
@@ -1,10 +1,42 @@
 package main.java.com.bookstore;
 
 import main.java.com.bookstore.service.LoginService;
+import main.java.com.bookstore.view.ProductManagementGUI;
 
-public class Main {
+import javax.swing.*;
+
+public class Main extends JFrame{
+	Main() {
+		setTitle("ERP");
+		setSize(1600,1200);
+		setMenuBar();
+		setVisible(true);
+	}
+
+	private void setMenuBar() {
+		JMenuBar jMenuBar = new JMenuBar();
+		JMenu management = new JMenu("management");
+
+		management.add(new JMenuItem("상품 관리"));
+		management.add(new JMenuItem("고객 관리"));
+		management.add(new JMenuItem("인사 관리"));
+		management.addSeparator();
+		management.add(new JMenuItem("Exit"));
+
+		jMenuBar.add(management);
+		jMenuBar.add(new JMenu("Edit"));
+		jMenuBar.add(new JMenu("샘플"));
+		jMenuBar.add(new JMenu("샘플"));
+		jMenuBar.add(new JMenu("샘플"));
+		setJMenuBar(jMenuBar);
+	}
+
 	public static void main(String[] args) {
-		LoginService ls = new LoginService();
-		ls.main(args);
+
+
+		new Main();
+
+		ProductManagementGUI pmg = new ProductManagementGUI();
+		pmg.main(args);
 	}
 }
