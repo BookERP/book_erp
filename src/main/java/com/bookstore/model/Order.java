e369f65f89ae32a4b100e153309ca600da065fc9
Justin-SJ-Hong <madwolves98@gmail.com>
2024-07-23 16:50:05 +0900

    aaaa

--- src/main/java/com/bookstore/model/Order.java
+++ src/main/java/com/bookstore/model/Order.java
@@ -1,4 +1,53 @@
-package src.main.java.com.bookstore.model;
-
-public class Order {
-}
+//package src.main.java.com.bookstore.model;
+//
+//import java.util.Date;
+//
+//public class Order {
+//	private String OrderID;
+//	private Date OrderDate;
+//	private Date ShippingDate;
+//	private String Status;
+//	private double amount;
+//	private String CID;
+//	public String getOrderID() {
+//		return OrderID;
+//	}
+//	public void setOrderID(String orderID) {
+//		OrderID = orderID;
+//	}
+//	public Date getOrderDate() {
+//		return OrderDate;
+//	}
+//	public void setOrderDate(Date orderDate) {
+//		OrderDate = orderDate;
+//	}
+//	public Date getShippingDate() {
+//		return ShippingDate;
+//	}
+//	public void setShippingDate(Date shippingDate) {
+//		ShippingDate = shippingDate;
+//	}
+//	public String getStatus() {
+//		return Status;
+//	}
+//	public void setStatus(String status) {
+//		Status = status;
+//	}
+//	public double getAmount() {
+//		return amount;
+//	}
+//	public void setAmount(double amount) {
+//		this.amount = amount;
+//	}
+//	public String getCID() {
+//		return CID;
+//	}
+//	public void setCID(String cID) {
+//		CID = cID;
+//	}
+//	@Override
+//	public String toString() {
+//		return "Order [OrderID=" + OrderID + ", OrderDate=" + OrderDate + ", ShippingDate=" + ShippingDate + ", Status="
+//				+ Status + ", amount=" + amount + ", CID=" + CID + "]";
+//	}
+//}
